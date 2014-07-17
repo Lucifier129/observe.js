91a9af56ee60b5a83790f97e8b90ceee9ac2ed81
Jade <Lucifier129@users.noreply.github.com>
2014-07-17 17:25:59 +0800

    Update jQuery.jPlus.js

--- jQuery.jPlus.js
+++ jQuery.jPlus.js
@@ -2,74 +2,79 @@
  Copyright 2014 Jade Gu
  http://weibo.com/islucifier
  Released under the MIT license
- refresh.js 2014.7.16
+ refresh.js 2014.7.17
  ==================================================*/
 ;(function($, window, document, undefined) {
 	//严格模式
 	'use strict';
 	var head = document.getElementsByTagName('head')[0],
 		observe,
-		//初始化，判断是否为现代浏览器
-		//将对象的属性拷贝到新对象上
-		//IE低版本浏览器，新对象为DOM对象
-		observe_init = (function() {
-			var init;
-			if ('create' in Object) {
-				observe = addSetter;
-				addEvent = null;
-				init = function(obj) {
-					var _obj,
-						_old,
-						key;
-					//对象的__hasSetter属性拥有被侦听属性的同名属性
-					//该同名属性将保存该属性变化时的callback值
-					if (!('__hasSetter' in obj)) {
-						_old = {};
-						_obj = obj;
-						obj = Object.create(proto);
-						for (key in _obj) {
-							_obj.hasOwnProperty(key) && (obj[key] = _old[key] = _obj[key]);
-						}
-						obj._old = _old;
-						obj.__hasSetter = {};
-					}
-					return obj;
-				};
-			} else if ('onpropertychange' in head) {
-				observe = addEvent;
-				addSetter = null;
-				var isIE6 = navigator.userAgent.indexOf("MSIE 6.0") > 0;
-				init = function(obj) {
-					var _obj,
-						_old,
-						key;
-					if (!('__hasSetter' in obj)) {
-						_old = {};
-						_obj = obj;
-						obj = document.createElement('obj');
-						head.appendChild(obj).parentNode.removeChild(obj);
-						//IE6删除DOM元素后，事件无法触发
-						isIE6 && head.appendChild(obj);
-						//拷贝参数对象中的属性
-						for (key in _obj) {
-							if (_obj.hasOwnProperty(key)) {
-								obj[key] = _old[key] = _obj[key];
+		method = {
+			//初始化，判断是否为现代浏览器
+			//将对象的属性拷贝到新对象上
+			//IE低版本浏览器，新对象为DOM对象
+			init: (function() {
+				var init;
+				if ('create' in Object) {
+					observe = addSetter;
+					addEvent = null;
+					init = function(obj) {
+						var _obj,
+							_old,
+							key;
+						//对象的__hasSetter属性拥有被侦听属性的同名属性
+						//该同名属性将保存该属性变化时的callback值
+						if (!('__hasSetter' in obj)) {
+							_old = {};
+							_obj = obj;
+							obj = Object.create(proto);
+							for (key in _obj) {
+								_obj.hasOwnProperty(key) && (obj[key] = _old[key] = _obj[key]);
 							}
+							obj._old = _old;
+							obj.__hasSetter = {};
 						}
-						//拷贝「原型」中的方法
-						for (key in proto) {
-							if (proto.hasOwnProperty(key)) {
-								obj[key] = proto[key];
+						return obj;
+					};
+				} else if ('onpropertychange' in head) {
+					observe = addEvent;
+					addSetter = null;
+					var isIE6 = navigator.userAgent.indexOf("MSIE 6.0") > 0;
+					init = function(obj) {
+						var _obj,
+							_old,
+							key;
+						if (!('__hasSetter' in obj)) {
+							_old = {};
+							_obj = obj;
+							obj = document.createElement('obj');
+							head.appendChild(obj).parentNode.removeChild(obj);
+							//IE6删除DOM元素后，事件无法触发
+							isIE6 && head.appendChild(obj);
+							//拷贝参数对象中的属性
+							for (key in _obj) {
+								if (_obj.hasOwnProperty(key)) {
+									obj[key] = _old[key] = _obj[key];
+								}
 							}
+							//拷贝「原型」中的方法
+							for (key in proto) {
+								if (proto.hasOwnProperty(key)) {
+									obj[key] = proto[key];
+								}
+							}
+							obj._old = _old;
+							obj.__hasSetter = {};
 						}
-						obj._old = _old;
-						obj.__hasSetter = {};
-					}
-					return obj;
-				};
+						return obj;
+					};
+				}
+				return init;
+			})(),
+			getRandomStr: function() {
+				return Math.random().toString(36).substr(2);
 			}
-			return init;
-		})(),
+		},
 		//被观察对象的原型
 		proto = {
 			//添加属性，_old属性与其同步更新
@@ -94,7 +99,7 @@
 			//遍历，简单循环
 			each: function(callback) {
 				for (var key in this._old) {
-					callback.call(this[key], key, this[key]);
+					callback.call(this, key, this[key]);
 				}
 				return this;
 			},
@@ -117,7 +122,7 @@
 						for (key in args) {
 							callback = args[key];
 							if (typeof callback === 'function') {
-								if (!(key in this)) {
+								if (!(key in this) && !(key.split('.')[0] in this)) {
 									this.add(key);
 								}
 								observe(this, key, callback);
@@ -133,7 +138,7 @@
 					key = args[0];
 					callback = args[1];
 					if (type === 'string' && typeof callback === 'function') {
-						if (!(key in this)) {
+						if (!(key in this) && !(key.split('.')[0] in this)) {
 							this.add(key);
 						}
 						observe(this, key, callback);
@@ -150,42 +155,72 @@
 				if (!args.length) {
 					this.__hasSetter = {};
 				} else {
-					args = Array.prototype.slice.call(args);
 					for (i = 0, len = args.length; i < len; i += 1) {
 						key = args[i];
-						typeof key === 'string' && key in this.__hasSetter && delete this.__hasSetter[key];
+						if (typeof key === 'string') {
+							if (key.indexOf('.') !== -1) {
+								if (key.indexOf('.') === 0) {
+									key = key.replace(/\./g, '');
+									for (var prop in this.__hasSetter) {
+										key in this.__hasSetter[prop] && delete this.__hasSetter[prop][key];
+									}
+								} else {
+									key = key.split('.');
+									key[1] in this.__hasSetter[key[0]] && delete this.__hasSetter[key[0]][key[1]];
+								}
+							} else if (key in this.__hasSetter) {
+								delete this.__hasSetter[key];
+							}
+						}
 					}
 				}
 				return this;
 			}
 		};
 
+
 	//在现代浏览器中，用Object.defineProperty实现属性侦听
 	function addSetter(obj, prop, callback) {
-		if (prop in obj.__hasSetter) {
-			obj.__hasSetter[prop] = callback;
-			return obj;
+		var name = prop.split('.'),
+			value;
+		prop = name[0];
+		name = name[1];
+		if (!(prop in obj.__hasSetter)) {
+			value = obj[prop];
+			obj.__hasSetter[prop] = {};
+			Object.defineProperty(obj, prop, {
+				set: function(v) {
+					value = v;
+					if (prop in this.__hasSetter) {
+						for (var key in this.__hasSetter[prop]) {
+							this.__hasSetter[prop][key].call(this, prop, v);
+						}
+					}
+				},
+				get: function() {
+					return value;
+				}
+			});
 		}
-		var value = obj[prop];
-		obj.__hasSetter[prop] = callback;
-		return Object.defineProperty(obj, prop, {
-			set: function(v) {
-				value = v;
-				prop in obj.__hasSetter && obj.__hasSetter[prop].call(obj, prop, v);
-			},
-			get: function() {
-				return value;
-			}
-		});
+		obj.__hasSetter[prop][name || 'observe-' + method.getRandomStr()] = callback;
+		return obj;
+
 	}
 	//在IE系列的落后浏览器中，用dom元素的onpropertychange事件来实现
 	function addEvent(obj, prop, callback) {
-		obj.__hasSetter[prop] = callback;
+		var name = prop.split('.');
+		prop = name[0];
+		name = name[1];
+		prop in obj.__hasSetter || (obj.__hasSetter[prop] = {});
+		obj.__hasSetter[prop][name || 'observe-' + method.getRandomStr()] = callback;
 		if (!obj.onpropertychange) {
-			var e = window.event;
-			obj.onpropertychange = function() {
-				var key = e.propertyName;
-				key in obj.__hasSetter && obj.__hasSetter[key].call(obj, key, obj[key]);
+			obj.onpropertychange = function(e) {
+				var prop = (e || window.event).propertyName;
+				if (prop in this.__hasSetter) {
+					for (var key in this.__hasSetter[prop]) {
+						this.__hasSetter[prop][key].call(this, prop, this[prop]);
+					}
+				}
 			};
 		}
 		//返回的新对象是一个DOM对象
@@ -193,10 +228,9 @@
 	}
 	//增强$函数，如果参数唯一，而且是朴素的js对象，则调用observe_init
 	//否则，调用jQuery的$函数
-	var _$ = $;
 	window.$ = function() {
 		var args = arguments;
-		return args.length === 1 && _$.isPlainObject(args[0]) ? observe_init.apply(null, args) : _$.apply(null, args);
+		return args.length === 1 && jQuery.isPlainObject(args[0]) ? method.init.apply(null, args) : jQuery.apply(null, args);
 	};
-	_$.extend(true, window.$, _$);
+	jQuery.extend(true, window.$, jQuery);
 })(jQuery, window, document);
